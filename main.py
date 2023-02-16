0579a39f04b6471542cdfaa53b62822a9b780b44
kuzhelstepan <82085853+kuzhelstepan@users.noreply.github.com>
2023-02-16 21:30:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 time.sleep(4)
 count=0
-while count<=100:
+while count<=36:
     pyautogui.typewrite('Long Live The Ottoman Empire')
     pyautogui.press("enter")
     count=count+1
